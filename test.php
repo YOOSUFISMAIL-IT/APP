c6f40d1069e207b048d0a1354e879af22b423341
YOOSUF ISMAIL <kiyas9148@gmail.com>
2025-08-19 00:22:21 +0530

    step1

---
+++ test.php
@@ -0,0 +1,22 @@
+<pre>
+  <?php
+  echo __LINE__;
+  print_r("_SERVER");
+  print_r($_SERVER);
+  print_r("_GET");
+  print_r($_GET);
+  print_r("_POST");
+  print_r($_POST);
+   print_r("_FILES");
+   print_r($_FILES);
+    print_r("_COOKIE");
+    print_r($_COOKIE);
+
+
+
+
+  echo __LINE__;
+
+ ?>
+</pre>
+
